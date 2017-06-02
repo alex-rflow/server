464e171849caad14a66101b6d93a3271b2a6326a
alex-rflow <vlad.alex44@outlook.com>
2017-06-03 00:54:28 +0300

    img.pre final

--- img.php
+++ img.php
@@ -20,7 +20,7 @@ $white = @imagecolorallocate($im, 255,255,255);
 // Время
 $time = date("H:i:s");
 // Вывод последнего пользователя
-@imagecopy($im, $stamp, 1336, 45, 0, 0, imagesx($stamp), imagesy($stamp));
+@imagecopy($im, $stamp, 1336, 45, 50, 50, imagesx($stamp), imagesy($stamp));
 
 // Вывод имени
 @imagettftext($im, 10, 0, 625, 25, $white, $path.'fonts/open-sans-bold.ttf',$UsersName);
