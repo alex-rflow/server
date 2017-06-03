9bd57d3c9e3238ef04f67c504492846820004aa6
alex-rflow <vlad.alex44@outlook.com>
2017-06-03 22:13:16 +0300

    test

--- img.php
+++ img.php
@@ -177,7 +177,7 @@ RoundingOff($last_subscribe_photo, 100,100);
 file_put_contents ('header/last_subscribe.png', $last_subscribe_photo);
 $user = @ImageCreateFromPNG($path.'header/last_subscribe.png');
 @imagettftext($im, 30, 0, 45, 350, $white, $path.'font/BebasNeue Regular.ttf',$time);
-@imagecopy($im, $user, 523, 105, 0, 0, 100, 100);
+@imagecopy($im, $user, 518, 105, 0, 0, 100, 100);
 // Вывод имени
 
 $text = $last_subscribe_firstname . ' ' . $last_subscribe_lastname;
