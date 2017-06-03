277f9f969ba4fa1fdc16c4c3cb95f7fdbec27d80
alex-rflow <vlad.alex44@outlook.com>
2017-06-03 22:35:40 +0300

    test

--- img.php
+++ img.php
@@ -185,7 +185,7 @@ $last_coment_user_photo = new Imagick($file_name2);
 RoundingOff($last_coment_user_photo, 100,100);
 file_put_contents ('header/last_coment_user.png', $last_coment_user_photo);
 $user = @ImageCreateFromPNG($path.'header/last_coment_user.png');
-@imagecopy($im, $user, 971.5, 102.5, 0, 0, 100, 100);
+@imagecopy($im, $user, 971.5, 102.5, 0, 0, 99, 99);
 // Вывод имени
 
 $text = $last_subscribe_firstname . ' ' . $last_subscribe_lastname;
