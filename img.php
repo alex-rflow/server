5d94151857b24fcbe879af7bd297ea48b465be41
alex-rflow <vlad.alex44@outlook.com>
2017-06-03 16:31:21 +0300

    test

--- img.php
+++ img.php
@@ -58,7 +58,7 @@ $user = @ImageCreateFromPNG($path.'header/last_subscribe.png');
 @imagecopy($im, $user, 725, 97, 0, 0, 140, 140);
 // Вывод имени
 $font = 'font/Tahoma.ttf';
-$text = 'Максим Соков';
+$text = 'Maxim Sokov';
 $fontwidth = imagefontwidth($font);
 
 $center = (imagesx($im)/2) - (7*strlen($text));
