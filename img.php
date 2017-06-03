d5302922a10ba2de1931b175d6319e58c1946a6f
alex-rflow <vlad.alex44@outlook.com>
2017-06-03 23:10:21 +0300

    pre

--- img.php
+++ img.php
@@ -182,10 +182,10 @@ $user = @ImageCreateFromPNG($path.'header/last_subscribe.png');
 
 $file_name2 = 'header/last_coment_user.jpg';
 $last_coment_user_photo = new Imagick($file_name2);
-RoundingOff($last_coment_user_photo, 98,98);
+RoundingOff($last_coment_user_photo, 99,99);
 file_put_contents ('header/last_coment_user.png', $last_coment_user_photo);
 $user2 = @ImageCreateFromPNG($path.'header/last_coment_user.png');
-@imagecopy($im, $user, 518, 104, 0, 0, 98, 98);
+@imagecopy($im, $user, 518, 103.5, 0, 0, 99, 99);
 @imagecopy($im, $user, 970.5, 103.5, 0, 0, 99, 99);
 // Вывод имени
 
