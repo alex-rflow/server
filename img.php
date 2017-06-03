54fecc2132df97c0dbb51b310f326e58439b4093
alex-rflow <vlad.alex44@outlook.com>
2017-06-03 21:50:36 +0300

    new

--- img.php
+++ img.php
@@ -90,7 +90,7 @@ function RoundingOff($_imagick, $width, $height) {
 }
 
 date_default_timezone_set("Europe/Moscow");
-$im = @ImageCreateFromJPEG ($path.'header/header.jpg');
+$im = @ImageCreateFromJPEG ($path.'header/header-2.jpg');
 
 
 // Аватар пользователя
