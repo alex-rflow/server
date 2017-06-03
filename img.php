c6b117f8836979f888fa23e66fe1b336c108f82d
alex-rflow <vlad.alex44@outlook.com>
2017-06-03 22:50:25 +0300

    test

--- img.php
+++ img.php
@@ -178,14 +178,14 @@ RoundingOff($last_subscribe_photo, 100,100);
 file_put_contents ('header/last_subscribe.png', $last_subscribe_photo);
 $user = @ImageCreateFromPNG($path.'header/last_subscribe.png');
 @imagettftext($im, 30, 0, 45, 350, $white, $path.'font/BebasNeue Regular.ttf',$time);
-@imagecopy($im, $user, 517.5, 105.5, 0, 0, 100, 100);
+@imagecopy($im, $user, 517.5, 102.5, 0, 0, 100, 100);
 
 $file_name2 = 'header/last_coment_user.jpg';
 $last_coment_user_photo = new Imagick($file_name2);
 RoundingOff($last_coment_user_photo, 97,97);
 file_put_contents ('header/last_coment_user.png', $last_coment_user_photo);
 $user = @ImageCreateFromPNG($path.'header/last_coment_user.png');
-@imagecopy($im, $user, 971.5, 102.5, 0, 0, 97, 97);
+@imagecopy($im, $user, 971.5, 105.5, 0, 0, 97, 97);
 // Вывод имени
 
 $text = $last_subscribe_firstname . ' ' . $last_subscribe_lastname;
