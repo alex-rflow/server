a48f6387c30d1413e4f329475258401d0f20887e
alex-rflow <vlad.alex44@outlook.com>
2017-06-04 14:36:59 +0300

    pre final

--- server2/img.php
+++ server2/img.php
@@ -1,8 +1,8 @@
 <?php
 
-// header('Content-type: image/png');
+header('Content-type: image/png');
 ini_set('display_errors', 1);
-require_once('api.php');
+require_once('../api.php');
 
 //Узнаем кто последний зашел в группу
 $last_subscribe = getApiMethod('groups.getMembers', array(
