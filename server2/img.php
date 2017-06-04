d3b843fc608e9320ffe6a8419b7934f63933b25e
alex-rflow <vlad.alex44@outlook.com>
2017-06-04 12:32:53 +0300

    test

--- server2/img.php
+++ server2/img.php
@@ -1,7 +1,7 @@
 <?php
 
-header('Content-type: image/png');
-// ini_set('display_errors', 1);
+// header('Content-type: image/png');
+ini_set('display_errors', 1);
 require_once('api.php');
 
 //Узнаем кто последний зашел в группу
