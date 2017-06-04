415069f37d6cd0fb765ae5979f0732766c6324da
alex-rflow <vlad.alex44@outlook.com>
2017-06-04 14:48:55 +0300

    server.js 2.2

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ function doRequest(url) {
 
 function start() {
 	doRequest(url);
-	setTimeout(doRequest(url1), 5000);
+	setTimeout(doRequest(url1), 2000);
 }
 
 setInterval(function() {start()}, 5000);
