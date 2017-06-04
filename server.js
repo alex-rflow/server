23ffbd1744bc3e9eeb0db0fe65dfb2a970d27e0a
alex-rflow <vlad.alex44@outlook.com>
2017-06-04 14:05:29 +0300

    server.js 2.0

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const http = require('http');
+var http = require('http');
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+server.listen(80);
+console.log("Server running at http://127.0.0.1:80/");
 
 var url = 'http://89.223.25.199/server/server1/';
 var url1 = 'http://89.223.25.199/server/server2/';
